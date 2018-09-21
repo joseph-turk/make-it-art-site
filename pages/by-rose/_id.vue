c41322d0811679c46c376a45d2cc2985e5af6a7d
Joseph Turk <joseph.r.turk@gmail.com>
2018-09-21 11:26:11 -0500

    Separate artwork purchase modal to its own component

--- pages/by-rose/_id.vue
+++ pages/by-rose/_id.vue
@@ -34,29 +34,19 @@
       </div>
     </section>
 
-    <div class="modal" :class="{ 'is-active': showModal }">
-      <div class="modal-background" @click="toggleModal"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Purchase</p>
-          <button class="delete" aria-label="close" @click="toggleModal"></button>
-        </header>
-        <section class="modal-card-body">
-          This will be the purchase form to take credit card and other info.
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button is-primary" @click="toggleModal">Buy Now</button>
-          <button class="button" @click="toggleModal">Cancel</button>
-        </footer>
-      </div>
-    </div>
+    <artwork-purchase-modal :showModal="showModal" @toggleModal="toggleModal" />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ArtworkPurchaseModal from '~/components/ArtworkPurchaseModal.vue'
 
 export default {
+  components: {
+    ArtworkPurchaseModal
+  },
+
   asyncData ({ params }) {
     return axios.get(`${process.env.apiUrl}/artwork/${params.id}`)
       .then(response => {
